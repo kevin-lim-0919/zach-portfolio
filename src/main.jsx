366d5ac1ed2720e0a921d0289f8d627e801b8e56
ruis <len.devteam@gmail.com>
2026-08-31 05:02:48 -0600

    Highlight software in hero title

--- src/main.jsx
+++ src/main.jsx
@@ -494,7 +494,7 @@ function App() {
                 Zachary Rosenberg · Principal Software Architect
               </div>
               <h1>
-                Turn <em>complex</em> systems into useful software
+                Turn <em>complex</em> systems into useful <em>software</em>
               </h1>
               <p>
                 I help organizations design, build, and operate reliable
